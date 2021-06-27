48481646823b803f9039fc548f5af8821f6cbbd4
Boning <me@boni.ng>
2021-06-27 19:11:33 +0100

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # [8 Queens Puzzle](https://en.wikipedia.org/wiki/Eight_queens_puzzle)
 OCaml Program to solve eight queens puzzle
+
+| Version | 5      | 6      | 7      | 8      | 9         |
+| -       | -      | -      | -      | -      | -         |
+| 1       | 0.033s | 0.040s | 0.164s | 3.164s | 2m41.547s |
--- src/eight_queens.ml
+++ src/eight_queens.ml
@@ -1,5 +1,3 @@
-let n = 8
-
 let rec rec_calculate cache (board : Board.t) =
   if Board.left board = 0 then
     let s = Board.to_string board in
@@ -22,21 +20,35 @@ let rec rec_calculate cache (board : Board.t) =
     done
 
 let caculate n =
-  let cache = Hashtbl.create 1 in
-  let _ = Hashtbl.add cache "count" 0 in
+  if n < 1 then
+    failwith "invalid input"
+  else if n = 1 then
+    Printf.printf "\n+---+\n| Q |\n+---+\n\nfundamental: 1    all: 1\n"
+  else if n = 2 || n = 3 then
+    Printf.printf "\nfundamental: 0    all: 0\n"
+  else
+    let cache = Hashtbl.create 1 in
+    let _ = Hashtbl.add cache "count" 0 in
 
-  let board = Board.make n in
-  let _ = ignore board in
-  let half = float_of_int n /. 2. |> ceil |> int_of_float |> fun i -> i - 1 in
-  for i = 0 to half do
-    for j = i to half do
-      match Board.place board i j with
-      | Some new_board -> rec_calculate cache new_board
-      | None -> failwith "the first placement should never fail"
-    done
-  done;
-  Printf.printf "\nfundamental: %d    all: %d\n"
-    (Hashtbl.find cache "count")
-    (Hashtbl.length cache - 1)
+    let board = Board.make n in
+    let half = float_of_int n /. 2. |> ceil |> int_of_float |> fun i -> i - 1 in
+    (* only put the first queen at the half of the upper left quarter *)
+    for i = 0 to half do
+      for j = i to half do
+        match Board.place board i j with
+        | Some new_board -> rec_calculate cache new_board
+        | None -> failwith "the first placement should never fail"
+      done
+    done;
+    Printf.printf "\nfundamental: %d    all: %d\n"
+      (Hashtbl.find cache "count")
+      (Hashtbl.length cache - 1)
 
-let () = caculate n
+let () =
+  let n =
+    if Array.length Sys.argv < 2 then
+      8
+    else
+      Sys.argv.(1) |> int_of_string
+  in
+  caculate n
